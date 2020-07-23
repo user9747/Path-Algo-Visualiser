c9c64c214c1e1b5db4326b4012b2b547bddc0042
user9747 <padinju@gmail.com>
2020-07-23 12:28:13 +0530

    build files moved out in gh

---
+++ precache-manifest.629791c8e890fc62ea750ffebe0479f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0d7f7fe4e6f8bdda6a0cc70a4cf8f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2740eb000893f42c88d6",
+    "url": "/static/css/main.36a7863a.chunk.css"
+  },
+  {
+    "revision": "e8f8a4eb6bbf000def60",
+    "url": "/static/js/2.4862d531.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4862d531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2740eb000893f42c88d6",
+    "url": "/static/js/main.0b89a2ff.chunk.js"
+  },
+  {
+    "revision": "05270871d403934f033d",
+    "url": "/static/js/runtime-main.3a3e0e65.js"
+  }
+]);
